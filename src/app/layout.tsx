d19d24b55cfe22ed94a8c354632481c899d1f876
xlaiglobal-ui <xlai.global@gmail.com>
2026-05-02 15:43:23 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
