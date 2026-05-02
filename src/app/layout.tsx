739661a7acb035c221f37968000b5eb6080bff86
xlaiglobal-ui <xlai.global@gmail.com>
2026-05-02 17:17:31 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="apple-mobile-web-app-title" content="XL AI " />
+        <meta name="apple-mobile-web-app-title" content="XLAI" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon0.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
